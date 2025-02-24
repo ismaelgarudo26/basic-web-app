2c6a6145a1d4eb974fbc4748f07aa99d080e06d4
ismael garudo <igarudo@andrew.cmu.edu>
2025-02-24 11:29:52 -0500

    Testing Vercel auto-deploy

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,4 +1,5 @@
 export default function QueryProcessor(query: string): string {
+  console.log("Deployment test");
   if (query.toLowerCase().includes("shakespeare")) {
     return (
       "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
